9654d7061c04896cdaebcacfa00a03fbac470cce
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-06-29 17:59:16 +0330

    update code

--- IdeaProjects/Clinic-system/src/Main.java
+++ IdeaProjects/Clinic-system/src/Main.java
@@ -1,3 +1,13 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+class GUi extends Application{
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
 
