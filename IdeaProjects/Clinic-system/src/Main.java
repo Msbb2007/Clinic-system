2b20146799a02a36b4a9870bfa3e7e8e476ca4de
Msbb2007 <mhmdsdrababazadh@gmail.com>
2026-06-29 18:01:03 +0330

    update code

--- IdeaProjects/Clinic-system/src/Main.java
+++ IdeaProjects/Clinic-system/src/Main.java
@@ -1,6 +1,8 @@
 import javafx.application.Application;
 import javafx.stage.Stage;
 
+class DatabaseManager{}
+
 class GUi extends Application{
     @Override
     public void start(Stage stage) throws Exception {
